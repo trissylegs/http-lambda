5efa219bf92d67f029e31528f90d66b5ab652870
Triss Healy <th@trissyle.gs>
2022-02-19 16:46:10 +1100

    Functioning Lamba HTTP endpoint.

--- lib/http-lambda-stack.ts
+++ lib/http-lambda-stack.ts
@@ -1,16 +1,32 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { DockerImage, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 
 export class HttpLambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'HttpLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const target = 'x86_64-unknown-linux-musl';
+    const hello = new Function(this, 'HelloHandler', {
+      code: Code.fromAsset('lambda/hello', {
+        bundling: {
+          command: [
+            'bash', '-c',
+            `rustup target add ${target} && ` + 
+            `cargo build --release --target ${target} && ` + 
+            `cp target/${target}/release/hello /asset-output/bootstrap`            
+          ],
+          image: DockerImage.fromRegistry('rust:1.57-slim')
+        }
+      }),
+      functionName: 'hello',
+      handler: 'main',
+      runtime: Runtime.PROVIDED_AL2
+    })
+    const gw = new LambdaRestApi(this, 'HelloEndpoint', {
+      handler: hello
+    });
   }
 }
